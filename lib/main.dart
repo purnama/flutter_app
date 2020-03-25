eaef0e97fdd3e7c379b0215a1f65b9aacc518eb7
Arthur Purnama <arthur@purnama.de>
2020-03-25 21:32:48 +0700

    complete tutorial 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() => runApp(MaterialApp(
-  title: 'Flutter App',
-  home: FlutterScaffold(),
-));
+      title: 'Flutter App',
+      home: FlutterScaffold(),
+    ));
 
-class FlutterScaffold extends StatefulWidget {
-
-  @override
-  _CounterState createState() {
-    return new _CounterState();
-  }
-}
-
-
-class _CounterState extends State<FlutterScaffold>{
-  int _counter = 0;
-
-  void _increment(){
-    setState(() {
-      ++_counter;
-    });
-  }
-
+class FlutterScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -42,22 +38,127 @@ class _CounterState extends State<FlutterScaffold>{
           )
         ],
       ),
-      body: Center(
-        child: Text(
-          'Pressed $_counter times',
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-            fontSize: 24.0,
-          ),
+      body: ListView(children: <Widget>[
+        Image.asset(
+          'images/lake.jpg',
+          fit: BoxFit.cover,
         ),
-      ),
+        titleSection,
+        buttonSection,
+        textSection,
+      ]),
       floatingActionButton: FloatingActionButton(
         tooltip: 'Add',
+        onPressed: () => print('TEEEEST'),
         child: Icon(Icons.add),
-        onPressed:_increment,
       ),
     );
   }
-
 }
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: () => setState(() {
+              if (_isFavorited) {
+                _favoriteCount -= 1;
+                _isFavorited = false;
+              } else {
+                _favoriteCount += 1;
+                _isFavorited = true;
+              }
+            }),
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Column _buildButtonColumn(Color color, IconData icon, String label) => Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          color: color,
+        ),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+
+Widget titleSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Row(
+    children: <Widget>[
+      Expanded(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Text(
+              'Kandersteg, Switzerland',
+              style: TextStyle(
+                color: Colors.green[500],
+              ),
+            )
+          ],
+        ),
+      ),
+      FavoriteWidget(),
+    ],
+  ),
+);
+
+Widget textSection = Container(
+  padding: const EdgeInsets.all(32),
+  child: Text(
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
